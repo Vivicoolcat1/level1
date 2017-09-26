4244f4207d0af855ec304cbdd3a8d6f9e28a34d6
Vivicoolcat1 <league@WTS21.attlocal.net>
2017-09-25 18:30:28 -0700

    ar[;'

--- src/level1/Houses.java
+++ src/level1/Houses.java
@@ -5,17 +5,17 @@ public static void main(String[] args) {
 	Robot r =new Robot();
 	 r.moveTo(0, 800);
 	r.penDown();
+	public void house() {
 r.turn(90);
 	r.move(10);
 	r.turn(270);
 	r.move(100);
-	r.turn(45);
-	r.move(20);
 	r.turn(90);
-	r.move(20);
-	r.turn(45);
+	r.move(40);
+	r.turn(90);
 	r.move(100);
-	
+	r.turn(-90);
+	r.move(20);}
 	
 	
 	
---
+++ src/level1/candyMan.java
@@ -0,0 +1,77 @@
+package level1;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.File;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Clip;
+import javax.swing.Icon;
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+public class candyMan implements ActionListener {
+	int timesClicked = 0;
+
+	public void runner() {
+		JPanel panel = new JPanel();
+		JFrame frame = new JFrame();
+		JButton button = new JButton("Candy Man");
+		frame.add(panel);
+		panel.add(button);
+		button.addActionListener(this);
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		if (timesClicked == 5) {
+			showPictureFromTheInternet("scary.jpg");
+			showPictureFromTheInternet("scream.wav");
+		} else {
+timesClicked ++;
+		}
+	}
+
+	private void showPictureFromTheInternet(String imageUrl) {
+		try {
+			URL url = new URL(imageUrl);
+			Icon icon = new ImageIcon(url);
+			JLabel imageLabel = new JLabel(icon);
+			JFrame frame = new JFrame();
+			frame.add(imageLabel);
+			frame.setVisible(true);
+			frame.pack();
+		} catch (MalformedURLException e) {
+			e.printStackTrace();
+		}
+
+	}
+
+	static final int CREEPY = 0;
+	static final int SCREAM = 1;
+
+	public static void playSound(int whichSound) {
+		File soundFile = null;
+		if (whichSound == CREEPY)
+			soundFile = new File("/Users/Guest/Google Drive/league-sounds/creepy-noise.wav");
+		else if (whichSound == SCREAM)
+			soundFile = new File("/Users/League/Google Drive/league-sounds/scream.wav");
+		try {
+			AudioInputStream audioInputStream = AudioSystem.getAudioInputStream(soundFile);
+			Clip clip = AudioSystem.getClip();
+			clip.open(audioInputStream);
+			clip.start();
+			Thread.sleep(3400);
+		} catch (Exception ex) {
+			ex.printStackTrace();
+		}
+	}
+
+}
