9ebc1170ccab744e9979dedfb427ad5ec21735df
League <League@AdministorsiMac.attlocal.net>
2017-11-06 18:29:11 -0800

    EasyButton,Popcorn,TypingTutor(IP)

--- src/level1/Popcorn.java
+++ src/level1/Popcorn.java
@@ -1,5 +1,73 @@
 package level1;
+//* Your mission and you have to accept it:
+	// *         Add a main method to the Popcorn class to cook a bag of Popcorn. Don't change the existing methods.
+	// */
 
-public class Popcorn {
+	class Microwave {
+		private int cookTime;
+		Popcorn thingToBeCooked;
 
+		Microwave() {
+			System.out.println("Microwave says: a Microwave has been made.");
+		}
+
+		void putInMicrowave(Popcorn thingToBeCooked) {
+			System.out.println("Microwave says: popcorn put in microwave.");
+			this.thingToBeCooked = thingToBeCooked;
+		}
+
+		void setTime(int cookTimeInMinutes) {
+			System.out.println("Microwave says: cook time is set to " + cookTime + " minutes.");
+			this.cookTime = cookTimeInMinutes;
+		}
+
+		void startMicrowave() {
+			if (thingToBeCooked == null)
+				System.out.println("Microwave says: there's nothing in the microwave!");
+			for (int i = 0; i < cookTime*10 + 1; i++) {
+				thingToBeCooked.applyHeat();
+			}
+		}
+	}
+
+	public class Popcorn {
+		
+public static void main(String[] args) {
+	Microwave m = new Microwave();
+		Popcorn pop = new Popcorn("BUTTER");
+		m.putInMicrowave(pop);
+		m.setTime(3);
+		m.startMicrowave();
+		pop.applyHeat();
 }
+		private int kernels = 20;
+		private String flavor;
+
+		Popcorn(String flavor) {
+			this.flavor = flavor;
+			System.out.println("Popcorn says: making package of " + this.flavor + " popcorn.");
+		}
+
+
+		public void applyHeat() {
+			pause();
+
+			if (kernels == 0) {
+				System.out.println("Popcorn says: Time to eat popcorn!");
+			} else {
+				System.out.println("POP!" + kernels);
+				kernels--;
+			}
+		}
+
+		private void pause() {
+			try {
+				Thread.sleep(150);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+		
+	}
+
+
---
+++ src/level1/TypingTutor.java
@@ -0,0 +1,26 @@
+package level1;
+
+import javax.swing.JFrame;
+
+public class TypingTutor {
+public static void main(String[] args) {
+	TypingTutor t = new TypingTutor();
+	t.run();
+}
+	void run() {
+		JFrame frame = new JFrame("Type or die");
+		frame.setVisible(true);
+		frame.pack();
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
