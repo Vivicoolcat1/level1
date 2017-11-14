83be84dbf6168a19e0f74c4cd3786465bce03ce0
League <League@AdministorsiMac.attlocal.net>
2017-11-13 18:31:33 -0800

    cfvbnm,./
    typingTutor,Whack A Mole (IP)

--- src/level1/TypingTutor.java
+++ src/level1/TypingTutor.java
@@ -1,20 +1,74 @@
 package level1;
 
-import javax.swing.JFrame;
+import java.awt.Color;
+import java.awt.Container;
+import java.awt.Frame;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.sql.Date;
+import java.util.Random;
 
-public class TypingTutor {
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+
+public class TypingTutor implements KeyListener {
+	char currentLetter; 
+	JLabel label = new JLabel();
+	JFrame frame = new JFrame("Type or die");
 public static void main(String[] args) {
 	TypingTutor t = new TypingTutor();
 	t.run();
+	
 }
 	void run() {
-		JFrame frame = new JFrame("Type or die");
+		
 		frame.setVisible(true);
 		frame.pack();
+		currentLetter = generateRandomLetter();
+		String a = Character.toString(currentLetter);
+		label.setText(a);
+		frame.add(label);
+		label.setFont(label.getFont().deriveFont(100.0f));
+	    label.setHorizontalAlignment(JLabel.CENTER);	
+frame.addKeyListener(this);
+	}	
+	
+	
+	char generateRandomLetter() {
+		Random r = new Random();
+		return (char) (r.nextInt(26) + 'a');
 	}
-	
-	
-	
+	@Override
+	public void keyTyped(KeyEvent e) {
+		// TODO Auto-generated method stub
+		if(e.getKeyChar() == currentLetter) {
+			System.out.println("correct");
+			Container c = frame.getContentPane();
+			c.setBackground(Color.green);
+			
+		}
+		else {
+			System.out.println("wrong");
+			Container c = frame.getContentPane();
+            c.setBackground(Color.red);
+		}
+	}
+	@Override
+	public void keyPressed(KeyEvent e) {
+		// TODO Auto-generated method stub
+		System.out.println("You typed " + currentLetter );
+	}
+	@Override
+	public void keyReleased(KeyEvent e) {
+		// TODO Auto-generated method stub
+	currentLetter = generateRandomLetter();
+
+	String a = Character.toString(currentLetter);
+	label.setText(a);
+	}
+ 
+
 	
 	
 	
---
+++ src/level1/wackAMole.java
@@ -0,0 +1,81 @@
+package level1;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+
+public class wackAMole {
+	JFrame frame = new JFrame();
+	JButton b = new JButton();
+	JButton c = new JButton();
+	JButton d = new JButton();
+	JButton e = new JButton();
+	JButton f = new JButton();
+	JButton g = new JButton();
+	JButton h = new JButton();
+	JButton i = new JButton();
+	JButton j = new JButton();
+	JButton k = new JButton();
+	JButton l = new JButton();
+	JButton m = new JButton();
+	JButton n = new JButton();
+	JButton o = new JButton();
+	JButton p = new JButton();
+	JButton q = new JButton();
+public static void main(String[] args) {
+	
+	wackAMole a = new wackAMole();
+	a.run();
+}
+void run() {
+	
+	frame.add(b);
+	frame.add(c);
+	frame.add(d);
+	frame.add(e);
+	frame.add(f);
+	frame.add(g);
+	frame.add(h);
+	frame.add(i);
+	frame.add(j);
+	frame.add(k);
+	frame.add(l);
+	frame.add(m);
+	frame.add(n);
+	frame.add(o);
+	frame.add(p);
+	frame.add(q);
+	frame.setVisible(true);
+	frame.pack();
+
+}
+
+
+void drawButtons() {
+
+	
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
